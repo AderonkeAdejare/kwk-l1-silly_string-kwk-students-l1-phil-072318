89c9e9d5334578248b774736969c06f13578c29e
AderonkeAdejare <aderonkea@comcast.net>
2018-07-25 13:03:32 +0000

    automatically backed up by learn

--- sillystring.rb
+++ sillystring.rb
@@ -1,7 +1,2 @@
-puts "music".upcase
-puts "IM NOT SHOUTING".downcase
-puts "supercalifragilisticexpialidocious".length
-puts "wow"+" mom"
-puts "i really like programming".capitalize
 sentence = "Astounding aardvarks, arguably an ancient animal, always ate apples and acorns and artichokes all around Athens, amazing!"
 sentence = sentence.gsub("a","o").gsub("A","O")
