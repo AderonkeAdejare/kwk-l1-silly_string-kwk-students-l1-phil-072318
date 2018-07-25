942f83bf35b58943c716e6b334786c4a1c4bab87
AderonkeAdejare <aderonkea@comcast.net>
2018-07-25 13:03:45 +0000

    automatically backed up by learn

--- sillystring.rb
+++ sillystring.rb
@@ -1,2 +1,2 @@
 sentence = "Astounding aardvarks, arguably an ancient animal, always ate apples and acorns and artichokes all around Athens, amazing!"
-sentence = sentence.gsub("a","o").gsub("A","O")
+sentence == sentence.gsub("a","o").gsub("A","O")
